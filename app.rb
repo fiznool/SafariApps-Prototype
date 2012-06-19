bafdfd3e0561186efe63fed1fa3398048994751e
Tom Spencer <tom.spencer@mubaloo.com>
2012-06-19 14:16:31 +0100

    Layout now accepts conditional to render search bar or not. Started place layout.

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@
 set:haml, :format => :html5
 
 get '/' do 
-  haml :places
+  haml :places, :locals => { :searchbar => true }
 end
 
-get '/:url' do
-  haml :"#{params[:url]}"
+get '/places' do
+  haml :places, :locals => { :searchbar => true }
+end
+
+get '/place' do
+  haml :place
 end
