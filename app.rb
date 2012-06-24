df4c9f2be14f03c1fe8c27e2246b715a43096c1f
fiznool <fiznool@gmail.com>
2012-06-24 17:32:23 +0100

    Wildlife details page and alternative wildlife list page

--- app.rb
+++ app.rb
@@ -14,6 +14,10 @@ get '/animals-2x1' do
   haml :animals_2x1, :locals => { :searchbar => true }
 end
 
+get '/animal' do
+  haml :animal
+end
+
 get '/places' do
   haml :places, :locals => { :searchbar => true }
 end
